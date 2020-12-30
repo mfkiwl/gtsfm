3e5c0dc1eced4e5d25d86170d75adebcf929f4a9
Ayush Baid <ayushrakeshbaid@gmail.com>
2020-12-30 19:02:43 +0530

    Adding ransac with eight point algorithm

--- gtsfm/frontend/verifier/ransac.py
+++ gtsfm/frontend/verifier/ransac.py
@@ -12,7 +12,7 @@ Authors: John Lambert
 import logging
 from typing import Optional, Tuple
 
-import cv2
+import cv2 as cv
 import numpy as np
 from gtsam import Cal3Bundler, Rot3, Unit3
 
@@ -23,13 +23,14 @@ from gtsfm.frontend.verifier.verifier_base import VerifierBase
 
 # minimum matches required for computing the E-matrix
 NUM_MATCHES_REQ_E_MATRIX = 5
-NORMALIZED_COORD_RANSAC_THRESH = 0.001 # TODO: hyperparameter to tune
-DEFAULT_RANSAC_SUCCESS_PROB = 0.999
+NUM_MATCHES_REQ_F_MATRIX = 7
+NORMALIZED_COORD_RANSAC_THRESH = 0.001  # TODO: hyperparameter to tune
+PIXEL_COORD_RANSAC_THRESH = 0.5  # TODO: hyperparameter to tune
+DEFAULT_RANSAC_SUCCESS_PROB = 0.9999
 
 
 def get_logger():
-    """
-    """
+    """"""
     logger_name = "main-logger"
     logger = logging.getLogger(logger_name)
     logger.setLevel(logging.INFO)
@@ -39,6 +40,7 @@ def get_logger():
         handler.setFormatter(logging.Formatter(fmt))
         logger.addHandler(handler)
     return logger
+
 
 logger = get_logger()
 
@@ -84,36 +86,35 @@ class Ransac(VerifierBase):
             return None, None, verified_indices
 
         uv_norm_i1 = feature_utils.normalize_coordinates(
-            keypoints_i1.coordinates,
-            camera_intrinsics_i1
+            keypoints_i1.coordinates, camera_intrinsics_i1
         )
         uv_norm_i2 = feature_utils.normalize_coordinates(
-            keypoints_i2.coordinates,
-            camera_intrinsics_i2
+            keypoints_i2.coordinates, camera_intrinsics_i2
         )
         K = np.eye(3)
 
-        i2Ei1, inlier_mask = cv2.findEssentialMat(
+        i2Ei1, inlier_mask = cv.findEssentialMat(
             uv_norm_i1[match_indices[:, 0]],
             uv_norm_i2[match_indices[:, 1]],
             K,
-            method=cv2.RANSAC,
+            method=cv.RANSAC,
             threshold=NORMALIZED_COORD_RANSAC_THRESH,
-            prob=DEFAULT_RANSAC_SUCCESS_PROB
+            prob=DEFAULT_RANSAC_SUCCESS_PROB,
         )
         inlier_idxs = np.where(inlier_mask.ravel() == 1)[0]
 
-        i2Ri1, i2Ui1 = \
-            verification_utils.recover_relative_pose_from_essential_matrix(
-                i2Ei1,
-                keypoints_i1.coordinates[match_indices[inlier_idxs, 0]],
-                keypoints_i2.coordinates[match_indices[inlier_idxs, 1]],
-                camera_intrinsics_i1,
-                camera_intrinsics_i2
-            )
+        (
+            i2Ri1,
+            i2Ui1,
+        ) = verification_utils.recover_relative_pose_from_essential_matrix(
+            i2Ei1,
+            keypoints_i1.coordinates[match_indices[inlier_idxs, 0]],
+            keypoints_i2.coordinates[match_indices[inlier_idxs, 1]],
+            camera_intrinsics_i1,
+            camera_intrinsics_i2,
+        )
 
         return i2Ri1, i2Ui1, match_indices[inlier_idxs]
-
 
     def verify_with_approximate_intrinsics(
         self,
@@ -142,5 +143,40 @@ class Ransac(VerifierBase):
             Indices of verified correspondences, of shape (N, 2) with N <= N3.
                 These indices are subset of match_indices.
         """
-        logger.info("Directly estimating essential matrix with approximate intrinsics not implemented")
-        raise NotImplementedError("Use Degensac instead for F-Matrix estimation")
+
+        verified_indices = np.array([], dtype=np.uint32)
+
+        # check if we don't have the minimum number of points
+        if match_indices.shape[0] < NUM_MATCHES_REQ_F_MATRIX:
+            return None, None, verified_indices
+
+        i2Fi1, mask = cv.findFundamentalMat(
+            keypoints_i1.coordinates[match_indices[:, 0]],
+            keypoints_i2.coordinates[match_indices[:, 1]],
+            method=cv.FM_RANSAC,
+            ransacReprojThreshold=PIXEL_COORD_RANSAC_THRESH,
+            confidence=DEFAULT_RANSAC_SUCCESS_PROB,
+            maxIters=10000,
+        )
+
+        if i2Fi1 is None:
+            return None, None, verified_indices
+
+        inlier_idxes = np.where(mask.ravel() == 1)[0]
+
+        i2Ei1_matrix = verification_utils.fundamental_to_essential_matrix(
+            i2Fi1, camera_intrinsics_i1, camera_intrinsics_i2
+        )
+
+        (
+            i2Ri1,
+            i2Ui1,
+        ) = verification_utils.recover_relative_pose_from_essential_matrix(
+            i2Ei1_matrix,
+            keypoints_i1.coordinates[match_indices[inlier_idxes, 0]],
+            keypoints_i2.coordinates[match_indices[inlier_idxes, 1]],
+            camera_intrinsics_i1,
+            camera_intrinsics_i2,
+        )
+
+        return i2Ri1, i2Ui1, match_indices[inlier_idxes]
